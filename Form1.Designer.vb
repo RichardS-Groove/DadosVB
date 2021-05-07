87263f576ca34ae330af0b9149cf18785bba4e26
RichardS <richardsjcb@gmail.com>
2021-05-07 18:05:23 -0300

    up Dados

---
+++ Form1.Designer.vb
@@ -0,0 +1,66 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Cubilete = New System.Windows.Forms.PictureBox()
+        Me.Dado = New System.Windows.Forms.PictureBox()
+        CType(Me.Cubilete, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Dado, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Cubilete
+        '
+        Me.Cubilete.BackColor = System.Drawing.Color.Transparent
+        Me.Cubilete.Location = New System.Drawing.Point(452, 20)
+        Me.Cubilete.Name = "Cubilete"
+        Me.Cubilete.Size = New System.Drawing.Size(323, 273)
+        Me.Cubilete.TabIndex = 1
+        Me.Cubilete.TabStop = False
+        '
+        'Dado
+        '
+        Me.Dado.BackgroundImage = Global.Dados.My.Resources.Resources.dado6
+        Me.Dado.Location = New System.Drawing.Point(540, 177)
+        Me.Dado.Name = "Dado"
+        Me.Dado.Size = New System.Drawing.Size(94, 95)
+        Me.Dado.TabIndex = 2
+        Me.Dado.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.Dados.My.Resources.Resources.mesa
+        Me.ClientSize = New System.Drawing.Size(784, 503)
+        Me.Controls.Add(Me.Dado)
+        Me.Controls.Add(Me.Cubilete)
+        Me.Name = "Form1"
+        Me.Text = "Apretá el Cubilete para tirar los dados"
+        CType(Me.Cubilete, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Dado, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Cubilete As PictureBox
+    Friend WithEvents Dado As PictureBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,44 @@
+﻿Public Class Form1
+
+    Dim Azar As Boolean = True
+    Private Sub Button1_Click_Click(sender As Object, e As EventArgs)
+        MessageBox.Show(NumDado(True))
+    End Sub
+
+    Function NumDado(Random As Boolean) As Integer
+        If Random Then Randomize()
+        Return Int(Rnd() * 6 + 1)
+    End Function
+
+    Private Sub Dado_Click(sender As Object, e As EventArgs) Handles Dado.Click
+        CambiaDado(Dado)
+    End Sub
+
+    Sub CambiaDado(ByRef DadoX As PictureBox)
+        Dim Num As Int16 = NumDado(True)
+        Select Case Num
+            Case 1 : DadoX.Image = My.Resources.dado1
+            Case 2 : DadoX.Image = My.Resources.dado2
+            Case 3 : DadoX.Image = My.Resources.dado3
+            Case 4 : DadoX.Image = My.Resources.dado4
+            Case 5 : DadoX.Image = My.Resources.dado5
+            Case 6 : DadoX.Image = My.Resources.dado6
+
+
+        End Select
+    End Sub
+
+
+    'Private Sub Button2_Click_Click(sender As Object, e As EventArgs) Handles Button2_Click.Click
+    '    Dim monto As Decimal = 1000
+    '    Dim IVA As Decimal = 0, IIBB As Decimal = 0, Interes1 As Decimal = 0
+    '    CalculaValores(monto, IVA, IIBB, Interes1)
+    '    MessageBox.Show("monto: " & monto & ", IVA: " & IVA & ", IIBB: " & IIBB & ", Interes1: " & Interes1)
+    'End Sub
+
+    'Sub CalculaValores(ByVal MontoX As Decimal, ByRef IVA As Decimal, ByRef IB As Decimal, ByRef Interes1 As Decimal)
+    '    IVA = MontoX * 21 / 100
+    '    IB = MontoX * 3 / 100
+    '    Interes1 = MontoX * 5 / 100
+    'End Sub
+End Class
